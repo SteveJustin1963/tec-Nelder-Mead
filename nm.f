66c715d73c4e7ab53d3b9472f0b6603bbbffc36b
Stephen Justin <stevejustin1963@gmail.com>
2023-07-16 10:14:51 +1000

    Update nm.f

--- nm.f
+++ nm.f
@@ -1,112 +1,123 @@
-64 CONSTANT N
-1.0 CONSTANT ALPHA
-2.0 CONSTANT GAMMA
-0.5 CONSTANT BETA
-0.5 CONSTANT DELTA
-1000 CONSTANT MAX-ITERATIONS
+\ Set constants
+64 CONSTANT N  \ Number of nodes
+1.0 CONSTANT ALPHA  \ Reflection parameter
+2.0 CONSTANT GAMMA  \ Expansion parameter
+0.5 CONSTANT BETA  \ Contraction parameter
+0.5 CONSTANT DELTA  \ Shrink parameter
+1000 CONSTANT MAX-ITERATIONS  \ Maximum number of iterations
 
+\ Define word to calculate square
 : square ( n -- n^2 ) dup * ;
+
+\ Define objective function
 : objective-func ( addr -- n ) dup @ square 2 * swap 1 + + ;
 
+\ Initialize the simplex
 : initialize-simplex ( addr -- )
   N 0 DO
-    DUP I 2 * 1E6 RND F!
+    DUP I 2 * 1E6 RND F!  \ Random initialization between 0 and 2 million
   LOOP ;
 
+\ Evaluate the objective function for each node in the simplex
 : evaluate-simplex ( addr1 addr2 -- )
   N 0 DO
-    DUP I @ objective-func I 4 * + F!
+    DUP I @ objective-func I 4 * + F!  \ Evaluate objective function for each node
   LOOP ;
 
+\ Order the simplex based on the function values
 : order-simplex ( addr1 addr2 -- )
   N 0 DO
     N 1 DO
       DUP I F@ DUP J F@ F> IF
-        SWAP I F! SWAP J F!
+        SWAP I F! SWAP J F!  \ Swap nodes if necessary based on function values
       THEN
     LOOP
   LOOP ;
 
+\ Perform the Nelder-Mead algorithm
 : nelder-mead ( -- )
-  N ALLOCATE DUP >R
-  N 4 * ALLOCATE DUP >R
+  N ALLOCATE DUP >R  \ Allocate memory for simplex
+  N 4 * ALLOCATE DUP >R  \ Allocate memory for function values
 
-  R@ initialize-simplex
-  R> R@ evaluate-simplex
-  R> R@ order-simplex
+  R@ initialize-simplex  \ Initialize the simplex
+  R> R@ evaluate-simplex  \ Evaluate the objective function
+  R> R@ order-simplex  \ Order the simplex
 
-  MAX-ITERATIONS 0 ?DO
+  MAX-ITERATIONS 0 ?DO  \ Loop until maximum iterations reached or convergence
+
+    \ Get simplex, best_node, worst_node, and second_worst_node
     R@ DUP R> R@ R> R@
 
     \ Compute the centroid (excluding the worst node)
     N 0 DO
-      DUP I @
+      DUP I @  \ Get current node
       N 1 DO
         DUP I @ DUP J @ <> IF
-          F+
+          F+  \ Sum the coordinates of all nodes except worst node
         THEN
       LOOP
-      DUP N 1- F/ FSWAP I F!
+      DUP N 1- F/ FSWAP I F!  \ Compute the centroid coordinates
     LOOP
 
     \ Reflect
     R@ ALLOCATE DUP >R
     N 0 DO
-      DUP I @ DUP I @ R> F@ R> F@ F- ALPHA F* F+ I F!
+      DUP I @ DUP I @ R> F@ R> F@ F- ALPHA F* F+ I F!  \ Compute reflected node coordinates
     LOOP
-    DUP R> F@ N 4 * + F!
+    DUP R> F@ N 4 * + F!  \ Compute reflected node function value
     R> F@ N 4 * + F> R> IF
-      R> FREE
-      R> DUP N 4 * + F!
+      R> FREE  \ Free memory for worst node
+      R> DUP N 4 * + F!  \ Store reflected node coordinates
     ELSE
-      R> FREE
-      R> DUP N 4 * + F!
+      R> FREE  \ Free memory for reflected node
+      R> DUP N 4 * + F!  \ Store worst node coordinates
     THEN
 
     \ Expand or Contract
     DUP F@ R> F@ F< IF
       R@ ALLOCATE DUP >R
       N 0 DO
-        DUP I @ DUP R> F@ DUP R> F@ F- GAMMA F* F+ I F!
+        DUP I @ DUP R> F@ DUP R> F@ F- GAMMA F* F+ I F!  \ Compute expanded node coordinates
       LOOP
-      DUP R> F@ N 4 * + F!
+      DUP R> F@ N 4 * + F!  \ Compute expanded node function value
       R> F@ N 4 * + F> R> IF
-        R> FREE
-        R> DUP N 4 * + F!
+        R> FREE  \ Free memory for worst node
+        R> DUP N 4 * + F!  \ Store expanded node coordinates
       ELSE
-        R> FREE
-        R> DUP N 4 * + F!
+        R> FREE  \ Free memory for expanded node
+        R> DUP N 4 * + F!  \ Store worst node coordinates
       THEN
     ELSE
       R@ ALLOCATE DUP >R
       N 0 DO
-        DUP I @ DUP R> F@ DUP R> F@ F- BETA F* F+ I F!
+        DUP I @ DUP R> F@ DUP R> F@ F- BETA F* F+ I F!  \ Compute contracted node coordinates
       LOOP
-      DUP R> F@ N 4 * + F!
+      DUP R> F@ N 4 * + F!  \ Compute contracted node function value
       R> F@ N 4 * + F> R> IF
-        R> FREE
+        R> FREE  \ Free memory for worst node
         N 1 DO
-          DUP I 2DUP @ 2DUP F@ F- DELTA F* F+ I F!
+          DUP I 2DUP @ 2DUP F@ F- DELTA F* F+ I F!  \ Compute shrunk node coordinates
         LOOP
       ELSE
         N 1 DO
-          DUP I 2DUP @ 2DUP F@ F- DELTA F* F+ I F!
+          DUP I 2DUP @ 2DUP F@ F- DELTA F* F+ I F!  \ Compute shrunk node coordinates
         LOOP
       THEN
     THEN
 
-    R@ R> order-simplex
-    LOOP
+    R@ R> order-simplex  \ Order the simplex
+  LOOP
 
   CR ." Best solution found: "
   N 0 DO
-    DUP I @ . FSPACE
+    DUP I @ . FSPACE  \ Print best solution coordinates
   LOOP
-  CR ." Objective function value: " DUP N 4 * + F@ .
+  CR ." Objective function value: " DUP N 4 * + F@ .  \ Print objective function value
   CR
 
-  FREE FREE ;
+  FREE FREE ;  \ Deallocate memory
 
+\ Main program
 : main ( -- )
   NELDER-MEAD ;
 
